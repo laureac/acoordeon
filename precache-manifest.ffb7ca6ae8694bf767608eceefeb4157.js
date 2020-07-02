28d0ffddb2f8854a6fc647361c0698d128159350
Laure Claret <laure.a.claret@gmail.com>
2020-07-02 14:55:30 +1000

    Updates

---
+++ precache-manifest.ffb7ca6ae8694bf767608eceefeb4157.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece06ef8357c622bdb8964f5cf04e0a4",
+    "url": "/acoordeon/index.html"
+  },
+  {
+    "revision": "d6d24ad5bd41730d182e",
+    "url": "/acoordeon/static/css/main.a8c05977.chunk.css"
+  },
+  {
+    "revision": "432ed313b63dfc804c52",
+    "url": "/acoordeon/static/js/2.bb005240.chunk.js"
+  },
+  {
+    "revision": "d2907550b0749ad10542a424f45133ae",
+    "url": "/acoordeon/static/js/2.bb005240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6d24ad5bd41730d182e",
+    "url": "/acoordeon/static/js/main.9a75d59c.chunk.js"
+  },
+  {
+    "revision": "bc9198117017cc90d80d",
+    "url": "/acoordeon/static/js/runtime-main.23638ccc.js"
+  }
+]);
